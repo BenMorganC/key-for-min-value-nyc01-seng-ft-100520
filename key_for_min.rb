0e218ac9e6bd3ffda22db509b614bf6b3f3487a9
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-22 14:30:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,12 @@
 def key_for_min_value(name_hash)
     counter = 0
+    
     while counter < name_hash.count do
         
         name_hash.each do |key, value|
-        
-            if name_hash[key][value] < smallest_value
-            smallest_value = name_hash[key][value]
+        smallest_value = name_hash[key][value]
+            if name_hash[counter][value] < smallest_value
+            smallest_value = name_hash[counter][value]
           end
             counter += 1 
         end
